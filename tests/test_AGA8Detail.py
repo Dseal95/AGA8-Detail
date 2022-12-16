67fbc1a93a1cbf7fd393c8521780b4dc4c3618d6
danielseal <dan.seal.06@gmail.com>
2022-12-16 16:05:08 +0000

    modifying test case with detail output (previous had properties output)

--- tests/test_AGA8Detail.py
+++ tests/test_AGA8Detail.py
@@ -2,39 +2,14 @@ import pytest
 from modules.AGA8Detail import AGA8Detail
 
 
-def test_AGA8DETAIL_C_Example():
+def test_AGA8_DETAIL_C_Example():
     """Test the Python implementation of AGA8 Detail method on the C++ example."""
     # inputs
     P = 50000  # Kpa
     T = 400  # K
-    x = [
-        0.0,
-        0.77824,
-        0.02,
-        0.06,
-        0.08,
-        0.03,
-        0.0015,
-        0.003,
-        0.0005,
-        0.00165,
-        0.00215,
-        0.00088,
-        0.00024,
-        0.00015,
-        0.00009,
-        0.004,
-        0.005,
-        0.002,
-        0.0001,
-        0.0025,
-        0.007,
-        0.001,
-    ]
+    x = [0.0, 0.77824, 0.02, 0.06, 0.08, 0.03, 0.0015, 0.003, 0.0005, 0.00165, 0.00215, 0.00088, 0.00024, 0.00015, 0.00009, 0.004, 0.005, 0.002, 0.0001, 0.0025, 0.007, 0.001]
 
-    actual = round(
-        AGA8Detail(P=P, T=T, x=x).run().Z, 7
-    )  # Allow for some rounding error due to random array initialisation.
-    desired = 1.1738014
+    actual = AGA8Detail(P=P, T=T, x=x).run().Z
+    desired = 1.1738013826890625
 
     assert actual == desired
