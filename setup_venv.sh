d59c7e9b52ab68f584fcc08238072c230727e980
danielseal <dan.seal.06@gmail.com>
2022-12-16 15:06:00 +0000

    adding gitignore file + .sh setup

---
+++ setup_venv.sh
@@ -0,0 +1,9 @@
+# set up venv using the following commands in a chosen dir
+
+python -m venv .venv
+source .venv/bin/activate
+
+python -m pip install -r requirements.txt
+
+# deactivate venv 
+$ deactivate
