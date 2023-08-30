2e6edb5d4106f8c20da5979b6d3d84be8761fe22
danielseal <dan.seal.06@gmail.com>
2023-08-30 18:20:19 +0100

    adding more unit tests

--- tests/test_AGA8Detail.py
+++ tests/test_AGA8Detail.py
@@ -5,7 +5,7 @@ from modules.AGA8Detail import AGA8Detail
 import pytest
 
 
-def test_aga8_detail_compressibility_factor_against_c():
+def test_aga8_detail_compressibility_factor_method_against_c():
     """Test the Python implementation of AGA8 Detail for approximating Z on the C++ example."""
     # inputs
     P = 50000  # Kpa
@@ -44,7 +44,7 @@ def test_aga8_detail_compressibility_factor_against_c():
     assert actual == desired
 
 
-def test_aga8_detail_properties_agaisnt_c():
+def test_aga8_detail_properties_method_against_c():
     """Test the Python implementation of AGA8 Detail properties method agaisnt C++ example.
 
     Note: There are some minor differences at higher precision due to the difference in instantiaion between C and Python.
@@ -110,5 +110,200 @@ def test_aga8_detail_properties_agaisnt_c():
         7.432969304794694e-05,
         2.6725092251846054,
     ]
+
+    assert actual == desired
+
+
+def test_aga8detail_z_approximation_example_1():
+    """Test AGA8Detail() approximation of z for a known example (from UniSim simulation) of P, T and x."""
+    # create test case
+    P = 11672.30591
+    T = 329.1959501
+    x = [
+        0.0,
+        0.8605181275,
+        0.00468943511,
+        0.021032297039999998,
+        0.0830931073,
+        0.024005787129999998,
+        0.00266965599,
+        0.00310500342,
+        0.00039002352999999997,
+        0.00023977192000000002,
+        3.197816e-05,
+        4.9399800000000004e-06,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+    # instantiate AGA8Detail class
+    AGA8 = AGA8Detail(p=P, t=T, x=x)
+
+    actual = AGA8.run().z
+    desired = 0.8453387504349907
+
+    assert actual == desired
+
+
+def test_aga8detail_z_approximation_example_2():
+    """Test AGA8Detail() approximation of z for a known example (from UniSim simulation) of P, T and x."""
+    # create test case
+    P = 11778.85981
+    T = 331.3596863
+    x = [
+        0.0,
+        0.8645574829,
+        0.00471616099,
+        0.02077505707,
+        0.08100759773,
+        0.02299965451,
+        0.0024834801,
+        0.00286307457,
+        0.00035400357,
+        0.00021567912000000001,
+        2.520598e-05,
+        2.6290599999999997e-06,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+    # instantiate AGA8Detail class
+    AGA8 = AGA8Detail(p=P, t=T, x=x)
+
+    actual = AGA8.run().z
+    desired = 0.8508947480602581
+
+    assert actual == desired
+
+
+def test_aga8detail_z_approximation_example_3():
+    """Test AGA8Detail() approximation of z for a known example (from UniSim simulation) of P, T and x."""
+    # create test case
+    P = 11133.88937
+    T = 331.0980687
+    x = [
+        0.0,
+        0.8616455105,
+        0.00469811792,
+        0.021060977329999998,
+        0.08256866667000001,
+        0.02373943755,
+        0.0026173493199999997,
+        0.0030427240999999997,
+        0.00037079653999999996,
+        0.00022267528000000002,
+        3.035669e-05,
+        3.37521e-06,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+    # instantiate AGA8Detail class
+    AGA8 = AGA8Detail(p=P, t=T, x=x)
+
+    actual = AGA8.run().z
+    desired = 0.853840503325755
+
+    assert actual == desired
+
+
+def test_aga8detail_z_approximation_example_4():
+    """Test AGA8Detail() approximation of z for a known example (from UniSim simulation) of P, T and x."""
+    # create test case
+    P = 11845.12799
+    T = 331.11875
+    x = [
+        0.0,
+        0.8627619880999999,
+        0.00491112452,
+        0.02085074657,
+        0.08144546933999999,
+        0.02354307161,
+        0.0026650497,
+        0.00307708257,
+        0.00039213710999999997,
+        0.00024061062,
+        3.428013e-05,
+        6.89542e-06,
+        8.67e-07,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+    # instantiate AGA8Detail class
+    AGA8 = AGA8Detail(p=P, t=T, x=x)
+
+    actual = AGA8.run().z
+    desired = 0.8490470421250338
+
+    assert actual == desired
+
+
+def test_aga8detail_z_approximation_example_5():
+    """Test AGA8Detail() approximation of z for a known example (from UniSim simulation) of P, T and x."""
+    # create test case
+    P = 11188.62433
+    T = 328.6182424000824
+    x = [
+        0.0,
+        0.8618463533,
+        0.00470927796,
+        0.02033212472,
+        0.08267388868999999,
+        0.02401060257,
+        0.0026570938599999997,
+        0.00309789558,
+        0.0003977758,
+        0.00023328679000000002,
+        3.573006e-05,
+        5.96658e-06,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+
+    # instantiate AGA8Detail class
+    AGA8 = AGA8Detail(p=P, t=T, x=x)
+
+    actual = AGA8.run().z
+    desired = 0.8482020334710543
 
     assert actual == desired
